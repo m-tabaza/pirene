fe6caf062098e593a3904d5078e236ec8698e0ef
Tabzz98 <muhammadtabaza@gmail.com>
2022-08-19 13:38:20 +0300

    Test Slinky hooks

--- client/src/main/scala/main.scala
+++ client/src/main/scala/main.scala
@@ -2,10 +2,24 @@ package pirene.client
 
 import org.scalajs.dom.document
 import slinky.core.*
+import slinky.core.facade.Hooks
 import slinky.web.ReactDOM
 import slinky.web.html.*
 
 @main def main = ReactDOM.render(
-  h1("Welcome to Pirene!"),
+  App.component("Welcome to Pirene"),
   document.getElementById("root")
 )
+
+object App {
+  type Props = String
+
+  val component = FunctionalComponent[Props] { props =>
+    val (count, setCount) = Hooks.useState(1)
+
+    div(
+      h1(props + "!" * count),
+      button(onClick := { () => setCount(count + 1) }, "Click me")
+    )
+  }
+}
