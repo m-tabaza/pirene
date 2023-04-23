33ba51fd16fc3ddde5b3ddc680d2fb12666f3124
Muhammad Tabaza <muhammadtabaza@gmail.com>
2023-04-23 18:22:09 +0300

    Add `exports.scala`

---
+++ core/src/main/scala/pirene/util/exports.scala
@@ -0,0 +1,17 @@
+package pirene.util
+
+export cats.Functor
+export cats.Applicative
+export cats.ApplicativeError
+export cats.ApplicativeThrow
+export cats.Monad
+export cats.MonadError
+export cats.MonadThrow
+export cats.Id
+export cats.Show
+export cats.Order
+export cats.data.NonEmptyList
+export cats.data.NonEmptySet
+export cats.data.EitherT
+export cats.data.OptionT
+export cats.implicits.*
